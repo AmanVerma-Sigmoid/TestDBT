052d0e26605c6ac0594a6146bf0114f44d01d752
amanv@sigmoidanalytics.com <amanv@sigmoidanalytics.com>
2022-07-07 10:55:02 +0000

    changes prod

--- macros/generte_schema_name.sql
+++ macros/generte_schema_name.sql
@@ -2,12 +2,11 @@
 
     {%- set default_schema = target.schema -%}
     {%- if custom_schema_name is none -%}
-
+        
         {{ default_schema }}
     {%- elif custom_schema_name is not none -%}  
-
-        {{ custom_schema_name }}  
-
+        {{ custom_schema_name }}
+           
     {%- else -%}
 
         {{ default_schema }}_{{ custom_schema_name | trim }}
